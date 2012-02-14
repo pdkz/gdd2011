19a04acb90fca960ac7b577690e07b29a9e41bbf
pdkz <pidekazu@gmail.com>
2012-02-14 15:52:49 +0900

    Update conn_spreadsheet.py

--- conn_spreadsheet.py
+++ conn_spreadsheet.py
@@ -19,40 +19,40 @@ class SpreadSheet :
     self.gd_client.ProgrammaticLogin()
 
   def set_document(self, name) :
-    self.document_name = name
-    self._get_current_key()
+     self.document_name = name
+     self._get_current_key()
     
   def _get_current_key(self) :
-    feed = self.gd_client.GetSpreadsheetsFeed()
-    for entry in feed.entry :
-      if entry.title.text == self.document_name :
-        self.current_key = entry.id.text.split('/')[-1]
-        break
-      else :
-        raise ValueError('document no found')
+     feed = self.gd_client.GetSpreadsheetsFeed()
+     for entry in feed.entry :
+       if entry.title.text == self.document_name :
+         self.current_key = entry.id.text.split('/')[-1]
+         break
+       else :
+         raise ValueError('document no found')
 
   def _get_worksheet_index(self) :
-    wksht_idx = len(self.gd_client.GetWorksheetsFeed(key=self.current_key).entry) - 1
-    #print "Worksheet Index:", wksht_idx
-    return wksht_idx
+     wksht_idx = len(self.gd_client.GetWorksheetsFeed(key=self.current_key).entry) - 1
+     #print "Worksheet Index:", wksht_idx
+     return wksht_idx
 
   def get_current_key(self) :
     return self.current_key
 
   def get_worksheet_id(self) :
-    wksht_idx = self._get_worksheet_index()
-    wksht_id = self.gd_client.GetWorksheetsFeed(key=self.current_key).entry[wksht_idx].id.text.rsplit('/', 1)[1]
+     wksht_idx = self._get_worksheet_index()
+     wksht_id = self.gd_client.GetWorksheetsFeed(key=self.current_key).entry[wksht_idx].id.text.rsplit('/', 1)[1]
  
-    #print "Worksheet ID:", wksht_id
-    return wksht_id
+     #print "Worksheet ID:", wksht_id
+     return wksht_id
 
   def create_worksheet(self, name) :
-    worksheet = self.gd_client.AddWorksheet(title=name, row_count=100, col_count=20, key=self.current_key)
+     worksheet = self.gd_client.AddWorksheet(title=name, row_count=100, col_count=20, key=self.current_key)
 
   def insert_row(self, data, wksht_id) :
-    for i, row in enumerate(data) :
-      for j, value in enumerate(row) :
-        self.gd_client.UpdateCell(row=i + 1, col=(j + 1), inputValue=value, key=self.current_key, wksht_id=wksht_id)
+     for i, row in enumerate(data) :
+       for j, value in enumerate(row) :
+         self.gd_client.UpdateCell(row=i + 1, col=(j + 1), inputValue=value, key=self.current_key, wksht_id=wksht_id)
 
 if __name__ == '__main__' :
   user = sys.argv[1:]
